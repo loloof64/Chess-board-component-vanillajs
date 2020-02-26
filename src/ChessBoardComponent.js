da8e41209cd0be3813954cff60d860bb382a425a
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-02-26 19:29:00 +0100

    Bug fixed: changing attributes at runtime also update the component

--- src/ChessBoardComponent.js
+++ src/ChessBoardComponent.js
@@ -1,3 +1,6 @@
+const defaultSizeAttr = '100.0';
+const defaultBackgroundAttr = '#124589';
+
 class ChessBoardComponent extends HTMLElement {
     constructor(){
         super();
@@ -7,9 +10,25 @@ class ChessBoardComponent extends HTMLElement {
     }
 
     connectedCallback() {
-       this.size = parseFloat(this.getAttribute('size') || '100.0');
-       this.backgroundColor = this.getAttribute('background') || '#124589';
+       this.size = parseFloat(this.getAttribute('size') || defaultSizeAttr);
+       this.backgroundColor = this.getAttribute('background') || defaultBackgroundAttr;
        this.render();
+    }
+
+    static get observedAttributes() {
+        return ['size', 'background'];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue == newValue) return;
+        if (name === 'size') {
+            this.size = parseFloat(newValue || defaultSizeAttr);
+            this.render();
+        }
+        else if (name === 'background') {
+            this.backgroundColor = newValue || defaultBackgroundAttr;
+            this.render();
+        }
     }
 
     render() {
