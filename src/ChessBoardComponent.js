5fc06dcf7e2856ea225d0a85e1a652a92b70d2d8
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-02-26 17:25:28 +0100

    Drawing the board background.

--- src/ChessBoardComponent.js
+++ src/ChessBoardComponent.js
@@ -1,6 +1,20 @@
 class ChessBoardComponent extends HTMLElement {
     constructor(){
         super();
+        this.attachShadow({ mode: 'open'});
+        this.shadowRoot.innerHTML = `
+            <style>
+                .root {
+                    width: 300px;
+                    height: 300px;
+                    background-color: #124589;
+                }
+            </style>
+
+            <div class="root">
+                
+            </div>
+        `;
     }
 
     connectedCallback() {
