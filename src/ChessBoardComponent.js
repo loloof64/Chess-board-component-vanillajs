77c694db14cd37a6aa2cdfe8633e4f734fe7e134
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-02-26 17:11:00 +0100

    Inital commit

---
+++ src/ChessBoardComponent.js
@@ -0,0 +1,11 @@
+class ChessBoardComponent extends HTMLElement {
+    constructor(){
+        super();
+    }
+
+    connectedCallback() {
+
+    }
+}
+
+customElements.define('loloof64-chessboard', ChessBoardComponent);
