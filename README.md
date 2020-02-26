bca02e407c5a138a0d8c3a8367db2803d60f23ec
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-02-26 20:00:08 +0100

    Drawing top coordinates around the board

--- README.md
+++ README.md
@@ -10,12 +10,14 @@ Usage
 <loloof64-chessboard
     size="222"
     background="crimson"
+    coordinatesColor="yellow"
 ></loloof64-chessboard>
 ```
 
 ### Properties
 
-| Name       | Role                                              | Type   | Default |
-|------------|---------------------------------------------------|--------|---------|
-| size       | Common size (width/height) of the board in pixels | number | 100.0   |
-| background | Background color of the board outside zone        | string | #124589 |
+| Name             | Role                                              | Type   | Default    |
+|------------------|---------------------------------------------------|--------|------------|
+| size             | Common size (width/height) of the board in pixels | number | 100.0      |
+| background       | Background color of the board outside zone        | string | #124589    |
+| coordinatesColor | Color of the coordinates around the board         | string | darkorange |
--- src/ChessBoardComponent.js
+++ src/ChessBoardComponent.js
@@ -1,5 +1,6 @@
 const defaultSizeAttr = '100.0';
 const defaultBackgroundAttr = '#124589';
+const defaultCoordinatesColorAttr = 'darkorange';
 
 class ChessBoardComponent extends HTMLElement {
     constructor(){
@@ -7,44 +8,91 @@ class ChessBoardComponent extends HTMLElement {
         this.attachShadow({ mode: 'open'});
         this.size;
         this.backgroundColor;
+        this.coordinatesColor;
     }
 
     connectedCallback() {
        this.size = parseFloat(this.getAttribute('size') || defaultSizeAttr);
        this.backgroundColor = this.getAttribute('background') || defaultBackgroundAttr;
-       this.render();
+       this.coordinatesColor = this.getAttribute('coordinatesColor') || defaultCoordinatesColorAttr;
+       this._render();
     }
 
     static get observedAttributes() {
-        return ['size', 'background'];
+        return ['size', 'background', 'coordinatesColor'];
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
         if (oldValue == newValue) return;
         if (name === 'size') {
             this.size = parseFloat(newValue || defaultSizeAttr);
-            this.render();
+            this._render();
         }
         else if (name === 'background') {
             this.backgroundColor = newValue || defaultBackgroundAttr;
-            this.render();
+            this._render();
+        }
+        else if (name === 'coordinatesColor') {
+            this.coordinatesColor = newValue || defaultCoordinatesColorAttr;
+            this._render();
         }
     }
 
-    render() {
+    _render() {
+        const cellsSize = this.size / 9.0;
+        const halfCellSize = cellsSize * 0.5;
+        const commonGridTemplate = `${halfCellSize}px repeat(8, ${cellsSize}px) ${halfCellSize}px`;
+
         this.shadowRoot.innerHTML = `
             <style>
                 .root {
+                    display: grid;
+                    grid-template: ${commonGridTemplate} / ${commonGridTemplate};
                     width: ${this.size}px;
                     height: ${this.size}px;
                     background-color: ${this.backgroundColor};
                 }
+
+                .coordinate {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    font-size: ${cellsSize * 0.5}px;
+                    color: ${this.coordinatesColor};
+                }
             </style>
 
             <div class="root">
-                
+                ${this._buildTopCells()}
+                ${this._buildMediumCells()}
+                ${this._buildBottomCells()}
             </div>
         `;
+    }
+
+    _buildTopCells() {
+        const coordinates = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+        const coordinatesCells = coordinates.map((letter, index) => {
+            return `
+                <div class="coordinate">
+                    ${letter}
+                </div>
+            `
+        });
+
+        return [
+            "<div></div>",
+            ...coordinatesCells.join(''),
+            "<div></div>"
+        ].join('');
+    }
+
+    _buildMediumCells() {
+
+    }
+
+    _buildBottomCells() {
+
     }
 }
 
