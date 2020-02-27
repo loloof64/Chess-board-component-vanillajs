f7bf38fedee4e8b288cc42597abe3a5e87ecbc3b
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-02-27 03:14:01 +0100

    Using babel transpiler and uglifier for Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,27 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
     mode: 'production',
     entry: './src/ChessBoardComponent.js',
     output: {
         filename: 'ChessBoardComponent.min.js',
         path: __dirname + '/dist',
-    }
+    },
+    optimization: {
+        minimizer: [new UglifyJsPlugin()],
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
 };
